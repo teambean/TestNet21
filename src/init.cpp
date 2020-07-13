e89aeccb21c7109e034821bf33668132a3f14e72
Shawn Kent <admin@bitbean.org>
2020-07-13 02:20:47 -0400

    Revert: Split non-GUI routines into its own

--- src/init.cpp
+++ src/init.cpp
@@ -68,34 +68,78 @@ bool ShutdownRequested()
     return fRequestShutdown;
 }
 
+void WaitForShutdown(boost::thread_group* threadGroup)
+{
+    bool fShutdown = ShutdownRequested();
+    // Tell the main threads to shutdown
+    while (!fShutdown)
+    {
+        MilliSleep(200);
+        fShutdown = ShutdownRequested();
+    }
+    if (threadGroup)
+    {
+        threadGroup->interrupt_all();
+        threadGroup->join_all();
+    }
+}
+
 void Shutdown()
 {
+    printf("%s: In progress...\n", __func__);
     static CCriticalSection cs_Shutdown;
     TRY_LOCK(cs_Shutdown, lockShutdown);
     if (!lockShutdown) return;
 
-    RenameThread("Beancash-shutoff");
+    /// Note: Shutdown() must be able to handle cases in which AppInit2() failed part of the way,
+        /// for example if the data directory was found to be locked.
+        /// Be sure that anything that writes files or flushes caches only does this if the respective
+        /// module was initialized.
+    // Make this thread recognisable as the shutdown thread
+    RenameThread("BitBean-shutoff");
     nTransactionsUpdated++;
     StopRPCThreads();
-    bitdb.Flush(false);
-    StopNode();
 
-    bitdb.Flush(true);
+    if (pwalletMain)
+        bitdb.Flush(false);
+    StopNode();
+    {
+        LOCK(cs_main);
+        if (pwalletMain)
+            pwalletMain->SetBestChain(CBlockLocator(pindexBest));
+    }
+
+    if (pwalletMain)
+        bitdb.Flush(true);
     boost::filesystem::remove(GetPidFile());
     UnregisterWallet(pwalletMain);
     delete pwalletMain;
+    printf("BitBean exited\n\n");
 
-    MilliSleep(50);
-    printf("Beancash exited\n\n");
-    #ifndef QT_GUI
-    // ensure non-UI client gets exited here, but let Beancash-qt reach 'return 0;' in beancash.cpp
-        exit(0);
-    #endif
+#ifndef QT_GUI
+    // ensure daemon is able to exit properly
+    exit(0);
+#endif
 }
 
 //
 // Signal handlers are very limited in what they are allowed to do
 //
+void DetectShutdownThread(boost::thread_group* threadGroup)
+{
+    bool fShutdown = ShutdownRequested();
+    // Tell the main threads to shutdown
+    while (!fShutdown)
+    {
+        MilliSleep(200);
+        fShutdown = ShutdownRequested();
+    }
+    if (threadGroup)
+    {
+        threadGroup->interrupt_all();
+        threadGroup->join_all();
+    }
+}
 
 void HandleSIGTERM(int)
 {
@@ -106,6 +150,135 @@ void HandleSIGHUP(int)
 {
     fReopenDebugLog = true;
 }
+
+//////////////////////////////////////////////////////////////////////////////
+//
+// Start
+//
+#if !defined(QT_GUI)
+bool AppInit(int argc, char* argv[])
+{
+    boost::thread_group threadGroup;
+    boost::thread* detectShutdownThread = NULL;
+    bool fRet = false;
+    fHaveGUI = false;
+    try
+    {
+        //
+        // Parameters
+        //
+        // If Qt is used, parameters/beancash.conf are parsed in qt/bitbean.cpp's main()
+        ParseParameters(argc, argv);
+        if (!boost::filesystem::is_directory(GetDataDir(false)))
+        {
+            fprintf(stderr, "Error: Specified data directory \"%s\" does not exist.\n", mapArgs["-datadir"].c_str());
+            return false;
+        }
+        ReadConfigFile(mapArgs, mapMultiArgs);
+        // Check for -testnet or -regtest parameter (TestNet() calls are only valid after this clause)
+        if (!SelectParamsFromCommandLine()) {
+            fprintf(stderr, "Error: Invalid combination of -regtest and -testnet used.\n");
+            return false;
+        }
+
+        if (mapArgs.count("-?") || mapArgs.count("--help"))
+        {
+            // First part of help message is specific to bitbeand / RPC client
+            std::string strUsage = _("Beancash version") + " " + FormatFullVersion() + "\n\n" +
+                _("Usage:") + "\n" +
+                  "  Beancashd [options]                     " + "\n" +
+                  "  Beancashd [options] <command> [params]  " + _("Send command to -server or Beancashd") + "\n" +
+                  "  Beancashd [options] help                " + _("List commands") + "\n" +
+                  "  Beancashd [options] help <command>      " + _("Get help for a command") + "\n";
+
+            strUsage += "\n" + HelpMessage();
+
+            fprintf(stdout, "%s", strUsage.c_str());
+            return false;
+        }
+
+        // Command-line RPC
+        bool fCommandLine = false;
+        for (int i = 1; i < argc; i++)
+            if (!IsSwitchChar(argv[i][0]) && !boost::algorithm::istarts_with(argv[i], "Beancash:"))
+                fCommandLine = true;
+
+        if (fCommandLine)
+        {
+            int ret = CommandLineRPC(argc, argv);
+            exit(ret);
+        }
+#ifndef WIN32
+    fDaemon = GetBoolArg("-daemon");
+    if (fDaemon)
+    {
+        // Daemonize
+        pid_t pid = fork();
+        if (pid < 0)
+        {
+            fprintf(stderr, "Error: fork() returned %d errno %d\n", pid, errno);
+            return false;
+        }
+        if (pid > 0) // Parent process, pid is child process ID
+        {
+            CreatePidFile(GetPidFile(), pid);
+            return true;
+        }
+        // Child process fall through to the rest of initialization
+
+        pid_t sid = setsid();
+        if (sid < 0)
+            fprintf(stderr, "Error: setsid() teturned %d errno %d\n", sid, errno);
+    }
+#endif
+
+        detectShutdownThread = new boost::thread(boost::bind(&DetectShutdownThread, &threadGroup));
+        fRet = AppInit2(threadGroup);
+    }
+    catch (std::exception& e) {
+        PrintException(&e, "AppInit()");
+    } catch (...) {
+        PrintException(NULL, "AppInit()");
+    }
+
+    if (!fRet)
+    {
+        if (detectShutdownThread)
+            detectShutdownThread->interrupt();
+
+        threadGroup.interrupt_all();
+
+
+    }
+
+    if (detectShutdownThread)
+    {
+        detectShutdownThread->join();
+        delete detectShutdownThread;
+        detectShudownThread = NULL;
+    }
+    Shutdown();
+
+    return fRet;
+}
+
+extern void noui_connect();
+int main(int argc, char *argv[])
+{
+    bool fRet = false;
+    fHaveGUI = false;
+
+    // Connect daemon signal handlers
+    noui_connect();
+
+    fRet = AppInit(argc, argv);
+
+    if (fRet && fDaemon)
+        return 0;
+
+    return (fRet ? 0 : 1);
+}
+#endif
 
 bool static InitError(const std::string &str)
 {

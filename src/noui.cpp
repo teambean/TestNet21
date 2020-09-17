b8bde6189e1c28cd6f422340bec0d852988c8677
Shawn Kent <admin@bitbean.org>
2020-09-17 02:08:08 -0400

    Remove unused paramater from ThreadSafeAskFee() and cleanup noui.cpp

--- src/noui.cpp
+++ src/noui.cpp
@@ -1,4 +1,3 @@
-
 // Copyright (c) 2010 Satoshi Nakamoto
 // Copyright (c) 2009-2012 The Bitcoin developers
 // Copyright (c) 2020 Bean Core www.beancash.org
@@ -28,19 +27,19 @@ static int noui_ThreadSafeMessageBox(const std::string &message, const std::stri
         strCaption += caption; // Use supplied caption (can be empty)
     }
 
-    LogPrintf("%s: %s\n", strCaption.c_str(), message.c_str());
+    LogPrintf("%s: %s\n", strCaption, message);
     fprintf(stderr, "%s: %s\n", strCaption.c_str(), message.c_str());
     return 4;
 }
 
-static bool noui_ThreadSafeAskFee(int64_t nFeeRequired, const std::string& strCaption)
+static bool noui_ThreadSafeAskFee(int64_t /*nFeeRequired*/)
 {
     return true;
 }
 
 static void noui_InitMessage(const std::string &message)
 {
-    LogPrintf("init message: %s\n", message.c_str());
+    LogPrintf("init message: %s\n", message);
 }
 
 void noui_connect()

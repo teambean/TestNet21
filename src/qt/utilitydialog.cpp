8572455545768814c85d35492a18eb640fbe89ef
Shawn Kent <admin@bitbean.org>
2020-11-16 09:07:52 -0500

    move defines to correct function HelpMessageDialog

--- src/qt/utilitydialog.cpp
+++ src/qt/utilitydialog.cpp
@@ -37,15 +37,6 @@ void AboutDialog::setModel(ClientModel *model)
 {
     if(model)
     {
-        QString version = model->formatFullVersion();
-        /* On x86 add a bit specifier to the version so that users can distinguish between
-         * 32 and 64 bit builds. On other architectures, 32/64 bit may be more ambigious.
-         */
-#if defined(__x86_64__)
-        version += " " + tr(" (%1-bit)").arg(64);
-#elif defined(__i386__ )
-        version += " " + tr(" (%1-bit)").arg(32);
-#endif
         ui->versionLabel->setText(version);
     }
 }
@@ -68,6 +59,16 @@ HelpMessageDialog::HelpMessageDialog(QWidget *parent) :
 {
     ui->setupUi(this);
     GUIUtil::restoreWindowGeometry("nHelpMessageDialogWindow", this->size(), this);
+
+    QString version = model->formatFullVersion();
+    /* On x86 add a bit specifier to the version so that users can distinguish between
+     * 32 and 64 bit builds. On other architectures, 32/64 bit may be more ambigious.
+     */
+#if defined(__x86_64__)
+    version += " " + tr("(%1-bit)").arg(64);
+#elif defined(__i386__ )
+    version += " " + tr("(%1-bit)").arg(32);
+#endif
 
     header = tr("Beancash-qt") + " " + tr("version") + " " +
         QString::fromStdString(FormatFullVersion()) + "\n\n" +
